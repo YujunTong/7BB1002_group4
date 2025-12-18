f2fe94bb716c4a6d6238a5c7714e2050f375ed8a
Kwan-nkh <ngkwanhim2@gmail.com>
2025-12-18 02:53:12 +0000

    Update star_alignment.sh

--- star_alignment.sh
+++ star_alignment.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-# Activate conda environment
+# Activate the environment
 
 export MAMBA_EXE=/scratch/grp/msc_appbio/Group4_ABCC/tools/micromamba
 eval "$($MAMBA_EXE shell hook --shell bash)"
-
-echo "sucessfully activate environment 1"
-
 micromamba activate /scratch/grp/msc_appbio/Group4_ABCC/envs/yeast_rnaseq_cli
 
 echo "successfully activate environment 2"
 
-
-
-
+# 
 Fastq_dir="/scratch/grp/msc_appbio/Group4_ABCC/output/faqc/pre_star_fastq"
 Output_dir="/scratch/grp/msc_appbio/Group4_ABCC/output/aligned"
 Index="/scratch/grp/msc_appbio/Group4_ABCC/raw_data/final_genome"
