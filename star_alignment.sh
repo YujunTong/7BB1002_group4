d566e6be52cb96e0014511c9a458068b7975470e
Kwan-nkh <ngkwanhim2@gmail.com>
2025-12-18 03:01:24 +0000

    Update star_alignment.sh

--- star_alignment.sh
+++ star_alignment.sh
@@ -8,12 +8,21 @@ micromamba activate /scratch/grp/msc_appbio/Group4_ABCC/envs/yeast_rnaseq_cli
 
 echo "successfully activate environment 2"
 
-# 
+# specify the directory for input fastq files, output, and the reference genome
 Fastq_dir="/scratch/grp/msc_appbio/Group4_ABCC/output/faqc/pre_star_fastq"
 Output_dir="/scratch/grp/msc_appbio/Group4_ABCC/output/aligned"
 Index="/scratch/grp/msc_appbio/Group4_ABCC/raw_data/final_genome"
 
 echo "start of STAR alignment"
+
+# Start of the loop for each fastq files
+# perform star alignment
+# use 4 threads
+# directory for genome reference
+# read compressed Fastq files
+# read the fastq file
+# use the fastq files name as prefix for the generated files
+# Output format and sorting
 
 for file in ${Fastq_dir}/*.fastq.gz
 do
@@ -29,12 +38,12 @@ do
          --outSAMtype BAM SortedByCoordinate
 
     echo "Indexing BAM for $sample"
-    samtools index ${Output_dir}/${sample}_Aligned.sortedByCoord.out.bam
 
 done
 
 echo "all STAR alignments completed"
 
+# To show STAR v.2.5.2a was used
 which STAR
 STAR --version
 
