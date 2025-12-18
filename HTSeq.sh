e39ee4197a6b4f6a1c5f95da219f6fb5821f5e9e
Kwan-nkh <ngkwanhim2@gmail.com>
2025-12-18 03:15:46 +0000

    Update HTSeq.sh

--- HTSeq.sh
+++ HTSeq.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 # Activate conda environment
-
 export MAMBA_EXE=/scratch/grp/msc_appbio/Group4_ABCC/tools/micromamba
 eval "$($MAMBA_EXE shell hook --shell bash)"
-
-echo "sucessfully activate environment 1"
-
 micromamba activate /scratch/grp/msc_appbio/Group4_ABCC/envs/yeast_rnaseq_cli
 
-echo "successfully activate environment 2"
+echo "successfully activate environment"
 
 
 echo "Starting HTSeq-count"
-
+# specify the directory for bam file, the annotation file, and the output directory
 Bam_dir="/scratch/grp/msc_appbio/Group4_ABCC/output/aligned"
 Gtf="/scratch/grp/msc_appbio/Group4_ABCC/raw_data/final_genome/annotation.gtf"
 Out_dir="/scratch/grp/msc_appbio/Group4_ABCC/output/htseq_counts"
 
+# create a new directory
 mkdir -p $Out_dir
+
+# Start of the loop
+# for all the bam files, 
+
 
 for bam in ${Bam_dir}/*_Aligned.sortedByCoord.out.bam
 do
