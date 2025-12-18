839ff0f4a919b1e4d20c4643eaa0e0f18354b27d
Kwan-nkh <ngkwanhim2@gmail.com>
2025-12-18 02:51:20 +0000

    Update star_copy_and_rename_fastq.sh

--- star_copy_and_rename_fastq.sh
+++ star_copy_and_rename_fastq.sh
@@ -11,7 +11,7 @@ mkdir -p pre_star_fastq
 # Step 3: for each folder (ERR4553381 to ERR4553400)
 for folder in ERR4553381 ERR4553382 ERR4553383 ERR4553384 ERR4553385 ERR4553386 ERR4553387 ERR4553388 ERR4553389 ERR4553390 ERR4553391 ERR4553392 ERR4553393 ERR4553394 ERR4553395 ERR4553396 ERR4553397 ERR4553398 ERR4553399 ERR4553400
 do
-    # Step 4: copy QC.unpaired.trimmed.fastq out of the folder
+    # Step 4: copy and rename QC.unpaired.trimmed.fastq out of the folder and paste it into the destination folder
     cp "$folder/QC.unpaired.trimmed.fastq.gz" "pre_star_fastq/${folder}_QC.unpaired.trimmed.fastq.gz"
 done
 
