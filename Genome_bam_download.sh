133a8ff509de2e3bd78616e1149b3e703d562508
Kwan-nkh <ngkwanhim2@gmail.com>
2025-12-18 06:38:58 +0000

    Add files via upload

---
+++ Genome_bam_download.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# Downloading the 10 bam files
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/err/ERR553/001/ERR5533591.bam
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/err/ERR553/001/ERR5533561.bam
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/err/ERR553/003/ERR5533563.bam
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/err/ERR553/006/ERR5533566.bam
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/err/ERR553/002/ERR5533562.bam
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/err/ERR553/004/ERR5533564.bam
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/err/ERR553/005/ERR5533565.bam
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/err/ERR553/004/ERR5533584.bam
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/err/ERR553/001/ERR5533581.bam
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/err/ERR553/003/ERR5533583.bam
+
+
